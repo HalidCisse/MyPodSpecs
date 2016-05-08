7960bd02ceb0489f310cbfd2590d348cdc1335c2
Halid Cisse <halidoucse@gmail.com>
2016-05-08 16:11:34 +0100

    Initial Commit

---
+++ MyPodSpecs/MyPodSpecs.m
@@ -0,0 +1,13 @@
+//
+//  MyPodSpecs.m
+//  MyPodSpecs
+//
+//  Created by Halid Cisse on 5/8/16.
+//  Copyright © 2016 Halid Cisse. All rights reserved.
+//
+
+#import "MyPodSpecs.h"
+
+@implementation MyPodSpecs
+
+@end
